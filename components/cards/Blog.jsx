0b71475e76ab37725bdb0b38e994abd04ca6afad
shaim <shaimmd8@gmail.com>
2022-02-13 22:15:19 +0530

    fixed minor errors

--- components/cards/Blog.jsx
+++ components/cards/Blog.jsx
@@ -18,7 +18,7 @@ export default function BlogCard({ title, thumbnail,desc, link }) {
           <img className="rounded-t-lg object-cover container max-h-48" src={thumbnail} alt="" />
           <div className="p-6">
             <h5 className={`${darktheme ? 'text-gray-50' : 'text-gray-900'} text-xl font-medium mb-2`}>{title}</h5>
-            <p class={`${darktheme?'text-gray-300':'text-gray-700'} text-base line-clamp-2`}>
+            <p className={`${darktheme?'text-gray-300':'text-gray-700'} text-base line-clamp-2`}>
               {totext(desc)}
             </p>
             <a target={'_blank'} rel='noreferrer noopener' href={link} className=" inline-block px-6 py-2.5 my-2 bg-pink-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg transition duration-150 ease-in-out">Read</a>
